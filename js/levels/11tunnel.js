ac718cee5d9e4921250fdaf00e4b7e86961bb769
Abelsoftware123 <abelsoftware123@hotmail.com>
2025-11-01 10:13:38 +0100

    Update 11tunnel.js

--- js/levels/11tunnel.js
+++ js/levels/11tunnel.js
@@ -5,29 +5,31 @@ var oneonetunnel = Mario.oneonetunnel = function() {
     background: "#000000",
     scrolling: false,
     coinSprite: function() {
-      return new Mario.Sprite('../../sprites/items.png', [0,96],[16,16], 6,[0,0,0,0,1,2,1]);
+      // CORRECTIE: Paden aangepast van '../../sprites' naar '../sprites'
+      return new Mario.Sprite('../sprites/items.png', [0,96],[16,16], 6,[0,0,0,0,1,2,1]);
     },
-    floorSprite:  new Mario.Sprite('../../sprites/tiles.png', [0,32],[16,16],0),
-    wallSprite: new Mario.Sprite('../../sprites/tiles.png', [32, 32],[16,16],0),
-    brickSprite: new Mario.Sprite('../../sprites/tiles.png', [16, 0], [16,16], 0),
-    brickBounceSprite: new Mario.Sprite('../../sprites/tiles.png',[32,0],[16,16],0),
-    ublockSprite: new Mario.Sprite('../../sprites/tiles.png', [48, 0], [16,16],0),
-    pipeLMidSprite: new Mario.Sprite('../../sprites/tiles.png', [0, 144], [16,16], 0),
-    pipeRMidSprite: new Mario.Sprite('../../sprites/tiles.png', [16, 144], [16,16], 0),
-    pipeLEndSprite: new Mario.Sprite('../../sprites/tiles.png', [0, 128], [16,16], 0),
-    pipeREndSprite: new Mario.Sprite('../../sprites/tiles.png', [16, 128], [16,16], 0),
-    pipeUpMid: new Mario.Sprite('../../sprites/tiles.png', [0, 144], [32,16], 0),
-    pipeSideMid: new Mario.Sprite('../../sprites/tiles.png', [48, 128], [16,32], 0),
-    pipeLeft: new Mario.Sprite('../../sprites/tiles.png', [32, 128], [16,32], 0),
-    pipeTop: new Mario.Sprite('../../sprites/tiles.png', [0, 128], [32,16], 0),
+    // CORRECTIE: Paden aangepast van '../../sprites' naar '../sprites'
+    floorSprite:  new Mario.Sprite('../sprites/tiles.png', [0,32],[16,16],0),
+    wallSprite: new Mario.Sprite('../sprites/tiles.png', [32, 32],[16,16],0),
+    brickSprite: new Mario.Sprite('../sprites/tiles.png', [16, 0], [16,16], 0),
+    brickBounceSprite: new Mario.Sprite('../sprites/tiles.png',[32,0],[16,16],0),
+    ublockSprite: new Mario.Sprite('../sprites/tiles.png', [48, 0], [16,16],0),
+    pipeLMidSprite: new Mario.Sprite('../sprites/tiles.png', [0, 144], [16,16], 0),
+    pipeRMidSprite: new Mario.Sprite('../sprites/tiles.png', [16, 144], [16,16], 0),
+    pipeLEndSprite: new Mario.Sprite('../sprites/tiles.png', [0, 128], [16,16], 0),
+    pipeREndSprite: new Mario.Sprite('../sprites/tiles.png', [16, 128], [16,16], 0),
+    pipeUpMid: new Mario.Sprite('../sprites/tiles.png', [0, 144], [32,16], 0),
+    pipeSideMid: new Mario.Sprite('../sprites/tiles.png', [48, 128], [16,32], 0),
+    pipeLeft: new Mario.Sprite('../sprites/tiles.png', [32, 128], [16,32], 0),
+    pipeTop: new Mario.Sprite('../sprites/tiles.png', [0, 128], [32,16], 0),
 
     LPipeSprites:[
-      new Mario.Sprite('../../sprites/tiles.png', [32,128],[16,16],0),
-      new Mario.Sprite('../../sprites/tiles.png', [32,144],[16,16],0),
-      new Mario.Sprite('../../sprites/tiles.png', [48,128],[16,16],0),
-      new Mario.Sprite('../../sprites/tiles.png', [48,144],[16,16],0),
-      new Mario.Sprite('../../sprites/tiles.png', [64,128],[16,16],0),
-      new Mario.Sprite('../../sprites/tiles.png', [64,144],[16,16],0),
+      new Mario.Sprite('../sprites/tiles.png', [32,128],[16,16],0),
+      new Mario.Sprite('../sprites/tiles.png', [32,144],[16,16],0),
+      new Mario.Sprite('../sprites/tiles.png', [48,128],[16,16],0),
+      new Mario.Sprite('../sprites/tiles.png', [48,144],[16,16],0),
+      new Mario.Sprite('../sprites/tiles.png', [64,128],[16,16],0),
+      new Mario.Sprite('../sprites/tiles.png', [64,144],[16,16],0),
     ]
 
   });
@@ -50,7 +52,7 @@ var oneonetunnel = Mario.oneonetunnel = function() {
     level.putCoin(pos[0],pos[1]);
   });
 
-  /level.putLeftPipe(13,11);
+  // level.putLeftPipe(13,11); // Commentaarregel gecorrigeerd
   level.putRealPipe(13,11,3,"RIGHT", function() {
     Mario.oneone.call();
     player.pos = [2616, 177]
