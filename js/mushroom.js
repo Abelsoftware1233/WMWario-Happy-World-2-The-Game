926f255755c83b632f2963fad2fb4a7b19112ef4
Abelsoftware123 <abelsoftware123@hotmail.com>
2025-11-01 10:33:06 +0100

    Update mushroom.js

--- js/mushroom.js
+++ js/mushroom.js
@@ -22,7 +22,7 @@
 
   Mushroom.prototype.spawn = function() {
     if (player.power > 0) {
-      /replace this with a fire flower
+      //replace this with a fire flower
       var ff = new Mario.Fireflower(this.pos)
       ff.spawn();
       return;
@@ -97,13 +97,13 @@
     this.isPlayerCollided();
   }
 
-  /we have access to player everywhere, so let's just do this.
+  //we have access to player everywhere, so let's just do this.
   Mushroom.prototype.isPlayerCollided = function() {
-    /the first two elements of the hitbox array are an offset, so let's do this now.
+    //the first two elements of the hitbox array are an offset, so let's do this now.
     var hpos1 = [this.pos[0] + this.hitbox[0], this.pos[1] + this.hitbox[1]];
     var hpos2 = [player.pos[0] + player.hitbox[0], player.pos[1] + player.hitbox[1]];
 
-    /if the hitboxes actually overlap
+    //if the hitboxes actually overlap
     if (!(hpos1[0] > hpos2[0]+player.hitbox[2] || (hpos1[0]+this.hitbox[2] < hpos2[0]))) {
       if (!(hpos1[1] > hpos2[1]+player.hitbox[3] || (hpos1[1]+this.hitbox[3] < hpos2[1]))) {
         player.powerUp(this.idx);
